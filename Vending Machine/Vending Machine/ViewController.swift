e7a4eef40b6a02b82b5e1722b0be6845f536097a
Chris Parkinson <chris@Chriss-iMac.local>
2020-06-28 16:17:36 -0400

    Initial commit with base xcode project

---
+++ Vending Machine/Vending Machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vending Machine
+//
+//  Created by Chris Parkinson on 6/28/20.
+//  Copyright © 2020 Chris Parkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
